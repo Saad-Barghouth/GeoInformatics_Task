5023b66fa7acc89845eb1faee564b6fbdc2039bf
S-Barghouth <149336703+Saad-Barghouth@users.noreply.github.com>
2024-12-18 02:17:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 
-
-   
-    // Highlight link on hover
-    const referenceLink = document.querySelector('.reference-link');
-    referenceLink.addEventListener('mouseover', () => {
-        referenceLink.style.color = "#0056b3"; // Highlight color on hover
-    });
-    referenceLink.addEventListener('mouseout', () => {
-        referenceLink.style.color = "#007bff"; // Original color when mouse leaves
-    });
-});
